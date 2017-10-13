af047ba585745a60e62d24a038164abf7192387d
liuqing <qliu@dataman-inc.com>
2017-10-13 15:32:19 +0800

    index2 --> tmp2

--- src/main/java/com/dataman/gitstats/configure/WebMvcConfiguration.java
+++ src/main/java/com/dataman/gitstats/configure/WebMvcConfiguration.java
@@ -1,6 +1,5 @@
 package com.dataman.gitstats.configure;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -8,13 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter{
-	
-	@Value("${gitstats.indexpath}")
-	private String indexpath;
+
 	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("redirect:/static/"+indexpath); // 添加首页
+		registry.addViewController("/").setViewName("redirect:/static/"); // 添加首页
 		// registry.addViewController("/manager").setViewName("redirect:/static/account.html"); // 添加管理页面
 	}
 	@Override
